0954d041c1d4dd16c56aae504c4490e3b8000765
ant <AnthonyMazz@users.noreply.github.com>
2019-03-18 12:10:45 -0400

    Added pysndfx module, to test effects on prerecorded guitar track.

--- ap.py
+++ ap.py
@@ -1,9 +1,40 @@
 import requests
 
+# Import the package and create an audio effects chain function.
+from pysndfx import AudioEffectsChain
+
+fx = (
+    AudioEffectsChain()
+    .reverb()
+    .phaser()
+    .delay()
+    .reverse()
+)
+
+infile = 'sessionAmpd.wav'
+outfile = 'processed_session.wav'
+
+# Apply phaser and reverb directly to an audio file.
+fx(infile, outfile)
+
+# Or, apply the effects directly to a ndarray.
+from librosa import load
+y, sr = load(infile, sr=None)
+y = fx(y)
+
+# Apply the effects and return the results as a ndarray.
+y = fx(infile)
+
+# Apply the effects to a ndarray but store the resulting audio to disk.
+fx(y, outfile)
 
 
+
+
+
+'''
 print("Hello World")
 my_ip=requests.get("http://bot.whatismyipaddress.com/")
 print(my_ip.text)
-
+'''
 
